08f06b3af29fd27e4af951eed9e34a55a787ba50
sergeyrid <sergeyrid@yandex.ru>
2020-09-25 11:49:49 +0300

    Add main and createSequence functions

--- src/main/kotlin/Fibonacci.kt
+++ src/main/kotlin/Fibonacci.kt
@@ -1,3 +1,13 @@
 fun main() {
-    print("Hello, 1 1 2 3 5 8!")
+    val n: Int? = readLine()?.toInt()
+    val fibSeq = createSequence(n)
+    print(fibSeq)
+}
+
+fun createSequence(n: Int?): List<Int>? {
+    return if (n != null) {
+        generateSequence(Pair(1, 1), { Pair(it.second, it.first + it.second) }).map {it.first}.take(n).toList()
+    } else {
+        null
+    }
 }
