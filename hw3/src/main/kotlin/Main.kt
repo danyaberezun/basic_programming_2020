291aadaefb381fce466c6b20ba3d93272c16fee4
receed <efremov-343@yandex.ru>
2020-09-26 20:43:55 +0300

    Add reverse

--- hw3/src/main/kotlin/Main.kt
+++ hw3/src/main/kotlin/Main.kt
@@ -1,14 +1,16 @@
-import kotlin.random.Random
-
-fun<T: Comparable<T>> quicksort(list: List<T>): List<T> {
+fun <T : Comparable<T>> quicksort(list: List<T>): List<T> {
     if (list.size <= 1)
         return list
-    val middle = list[Random.nextInt(list.size)]
+    val middle = list.random()
     val (equal, notEqual) = list.partition { it == middle }
     val (less, greater) = notEqual.partition { it < middle }
     return quicksort(less) + equal + quicksort(greater)
 }
 
+fun <T> reverse(list: List<T>): List<T> = list.foldRight(listOf()) { element, current -> current + element }
+
 fun main() {
     println(quicksort(listOf(1, 9, 2, 7, 4, 2, 3)))
+    println(reverse(listOf(1, 9, 4)))
+    println(reverse(listOf<Int>()))
 }
