c92119f009bde372cb3f72c2ea97e8c4a8368ae8
receed <efremov-343@yandex.ru>
2020-09-26 20:49:02 +0300

    Add filter

--- hw3/src/main/kotlin/Main.kt
+++ hw3/src/main/kotlin/Main.kt
@@ -9,8 +9,22 @@ fun <T : Comparable<T>> quicksort(list: List<T>): List<T> {
 
 fun <T> reverse(list: List<T>): List<T> = list.foldRight(listOf()) { element, current -> current + element }
 
+fun <T> filter(list: List<T>, f: (T) -> Boolean): List<T> = list.fold(listOf()) { current, element ->
+    if (f(element))
+        current + element
+    else
+        current
+}
+fun <T> filterRight(list: List<T>, f: (T) -> Boolean): List<T> = list.foldRight(listOf()) { element, current ->
+    if (f(element))
+        listOf(element) + current
+    else
+        current
+}
 fun main() {
     println(quicksort(listOf(1, 9, 2, 7, 4, 2, 3)))
     println(reverse(listOf(1, 9, 4)))
     println(reverse(listOf<Int>()))
+    println(filter(listOf(2, 5, 4, 1, 80, 2)) { it % 2 == 0 })
+    println(filterRight(listOf(2, 5, 4, 1, 80, 2)) { it % 2 == 0 })
 }
