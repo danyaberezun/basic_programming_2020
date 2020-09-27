502b700557ec98d77729706da92a245d5b378007
Kirill Kondratyuk <kirkondratiuk@gmail.com>
2020-09-27 19:35:27 +0300

    added DOC

--- fibonacci/src/Fibonacci.kt
+++ fibonacci/src/Fibonacci.kt
@@ -1,10 +1,13 @@
+
 fun main(args : Array<String>) {
+    // Проверка аргументов
     if (args.size > 1) error("Too many args")
     if (args.isEmpty()) error("Too few args")
-    if (args[0].toIntOrNull() == null) error("Argument should provide number")
+    if (args[0].toIntOrNull() == null || (args[0].toInt() < 0)) error("Argument should provide number above zero")
     println(fib(args[0].toInt()))
 }
 
+// Рекурсивный Фибоначи
 fun fib(n : Int) : Int {
     if (n == 0) return 0
     if (n == 1) return 1
