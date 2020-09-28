c8f8fe9975486d76c7a7e651a6905f48f5878674
Alexander Khoroshavin <alexander@khoroshav.in>
2020-09-28 23:31:16 +0300

    Add unit tests

--- src/test/kotlin/FibonacciTest.kt
+++ src/test/kotlin/FibonacciTest.kt
@@ -1,11 +1,32 @@
 package me.tests
 
+import me.fibonacci
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
 
-internal class Foo {
+internal class FibonacciTest {
 
     @Test
-    fun bar() {
-        assert(false)
+    fun positiveNumber() {
+        val number = 5
+        val expected = 5
+        val actual: Int = fibonacci(number)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun zero() {
+        val number = 0
+        val expected = 0
+        val actual: Int = fibonacci(number)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun negativeNumber() {
+        val number = -16
+        val expected = -987
+        val actual: Int = fibonacci(number)
+        assertEquals(expected, actual)
     }
 }
