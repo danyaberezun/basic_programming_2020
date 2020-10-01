e41ddecec6fbc43a5f8ecb4b23389de5c9703c73
sergeyrid <sergeyrid@yandex.ru>
2020-10-01 18:47:54 +0300

    Add filter function

--- src/main/kotlin/QuickSort.kt
+++ src/main/kotlin/QuickSort.kt
@@ -1,3 +1,8 @@
 fun reverse(array: List<Int>): List<Int> {
     return array.foldRight(emptyList(), { element, newArray -> newArray.plus(element) })
 }
+
+fun filter(array: List<Int>, predicate: (Int) -> (Boolean)): List<Int> {
+    return array.fold(emptyList(), { newArray, element ->
+        if (predicate(element)) newArray.plus(element) else newArray })
+}
