eb0451d88834b9ae1ca63e90bde3c1e78f92c396
Jacob Payne <jacob@daohub.io>
2020-10-22 18:40:52 +0300

    Fixed Function Signatures

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,7 +4,27 @@ package foo
 import java.lang.RuntimeException
 
 fun qsort(input: List<Int>): List<Int> {
+    return input
+}
 
+fun reverse(input: List<Int>): List<Int> {
+    return input
+}
+
+fun filter(input: List<Int>): List<Int> {
+    return input
+}
+
+fun filter_r(input: List<Int>): List<Int> {
+    return input
+}
+
+fun sumq(input: Int): Int {
+    return 0
+}
+
+fun mapAccumL(f: (Int, Int) -> Pair<Int, Int>, x: Int, y: List<Int> ): Pair<Int, List<Int>> {
+    return Pair(0, listOf())
 }
 
 fun main() {
