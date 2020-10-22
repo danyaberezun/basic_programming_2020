f5455f33267e0ec006cf8dbae9018238ea32f3f4
Jacob Payne <jacob@daohub.io>
2020-10-22 21:18:51 +0300

    Finished all tasks and completed tests

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -44,23 +44,48 @@ fun qsort(input_s: List<Int>): List<Int> {
 
 
 fun reverse(input: List<Int>): List<Int> {
-    return input
+    var res = mutableListOf<Int>()
+    return input.foldRight(res,{ i,r -> r.add(i); r });
 }
 
-fun filter(input: List<Int>): List<Int> {
-    return input
+fun filter(input: List<Int>, f: (Int)-> Boolean): List<Int> {
+    var res = mutableListOf<Int>()
+    return input.fold(res, fun (r: MutableList<Int>, i): MutableList<Int> {
+        if (f(i)) { r.add(i) }
+        return r;
+    });
 }
 
-fun filter_r(input: List<Int>): List<Int> {
-    return input
+fun filter_r(input: List<Int>, f: (Int)-> Boolean): List<Int> {
+    var res = mutableListOf<Int>()
+    return input.foldRight(res, fun (i, r: MutableList<Int>): MutableList<Int> {
+        if (f(i)) { r.add(0, i) }
+        return r;
+    });
+}
+
+fun countLines(input: List<String>): List<Int> {
+    return input.map { it.length }
 }
 
 fun sumq(input: Int): Int {
-    return 0
+    var acc = 0;
+    for (i in 0..input) {
+        acc += i*i;
+    }
+    return acc;
 }
 
-fun mapAccumL(f: (Int, Int) -> Pair<Int, Int>, x: Int, y: List<Int> ): Pair<Int, List<Int>> {
-    return Pair(0, listOf())
+fun mapAccumL( x: Int, y: List<Int>, f: (Int, Int) -> Pair<Int, Int>): Pair<Int, List<Int>> {
+    var x_result = x;
+    var y_result = mutableListOf<Int>()
+
+    for (yv in y) {
+        val (xn, yn) = f(x_result, yv);
+        x_result = xn;
+        y_result.add(yn);
+    }
+    return Pair(x_result, y_result);
 }
 
 fun main() {
--- src/test/kotlin/mainTest.kt
+++ src/test/kotlin/mainTest.kt
@@ -25,13 +25,20 @@ internal class mainTest {
     @Test
     fun `filter should filter a full array`() {
         var x = listOf(0, 243, 2, 3, 4, 3, 3, 5, 3, 4, 2, 4234, 433, 22, -10)
-        assertEquals(x.filter { it > 2 }, foo.filter(x))
+        assertEquals(x.filter { it > 2 }, foo.filter(x, { it > 2}))
     }
     @Test
-    fun `filter_r should reverse a full array`() {
+    fun `filter_r should filter a full array`() {
         var x = listOf(0, 243, 2, 3, 4, 3, 3, 5, 3, 4, 2, 4234, 433, 22, -10)
-        assertEquals(x.filter { it > 2 }, foo.filter_r(x))
+        assertEquals(x.filter { it > 2 }, foo.filter_r(x, { it > 2}))
     }
+
+    @Test
+    fun `countLines should count a full array`() {
+        var x = listOf("Bla", "");
+        assertEquals(listOf(3,0), foo.countLines(x))
+    }
+
     @Test
     fun `sumq should return a sum of array`() {
         var x = listOf(1,2,3,4,5,6).fold(0, { acc: Int, i: Int -> acc + (i*i) })
@@ -40,13 +47,13 @@ internal class mainTest {
 
     @Test
     fun `mapAccumL should return a sum of array`() {
-        val x1 = listOf(9,6,3);
-        val y1 = { x: Int, y: Int -> Pair(x, x*y)}
-        assertEquals(Pair(5, listOf(45, 30, 15)), foo.mapAccumL(y1, 5, x1))
+        val y1 = listOf(9,6,3);
+        val f1 = { x: Int, y: Int -> Pair(x, x*y)}
+        assertEquals(Pair(5, listOf(45, 30, 15)), foo.mapAccumL(5, y1, f1))
 
-        val x2 = listOf(2,4,8);
-        val y2 = { x: Int, y: Int -> Pair(x+y, x*y)}
-        assertEquals(Pair(19,listOf(10,28,88)), foo.mapAccumL(y2, 5, x2))
+        val y2 = listOf(2,4,8);
+        val f2 = { x: Int, y: Int -> Pair(x+y, x*y)}
+        assertEquals(Pair(19,listOf(10,28,88)), foo.mapAccumL(5, y2, f2))
     }
 
 
