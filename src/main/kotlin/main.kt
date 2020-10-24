715368f6e433aac6a72c9400d7725fbc06e9d7ed
Stephen Ostapenko <stepanos2002@gmail.com>
2020-10-25 00:06:57 +0300

    task 5 done again

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,19 @@
 @file: JvmName("main")
 
 interface Expr {
-    fun calc(): Int
-    override fun toString(): String
+    fun apply(v: PrintVisitor): String
+    fun apply(v: CalculateVisitor): Int
 }
 
 class Var(x: Int) : Expr {
     val value = x
 
-    override fun calc(): Int {
-        return value
+    override fun apply(v: PrintVisitor): String {
+        return v.visit(this)
     }
 
-    override fun toString(): String {
-        return "$value"
+    override fun apply(v: CalculateVisitor): Int {
+        return v.visit(this)
     }
 }
 
@@ -21,12 +21,12 @@ class Sum(a: Expr, b: Expr) : Expr {
     val left = a
     val right = b
 
-    override fun calc(): Int {
-        return left.calc() + right.calc()
+    override fun apply(v: PrintVisitor): String {
+        return v.visit(this)
     }
 
-    override fun toString(): String {
-        return "($left + $right)"
+    override fun apply(v: CalculateVisitor): Int {
+        return v.visit(this)
     }
 }
 
@@ -34,22 +34,50 @@ class Mul(a: Expr, b: Expr) : Expr {
     val left = a
     val right = b
 
-    override fun calc(): Int {
-        return left.calc() * right.calc()
+    override fun apply(v: PrintVisitor): String {
+        return v.visit(this)
     }
 
-    override fun toString(): String {
-        return "$left * $right"
+    override fun apply(v: CalculateVisitor): Int {
+        return v.visit(this)
+    }
+}
+
+class PrintVisitor {
+    fun visit(e: Var): String {
+        return "${e.value}"
+    }
+
+    fun visit(e: Sum): String {
+        return "(${e.left.apply(PrintVisitor())} + ${e.right.apply(PrintVisitor())})"
+    }
+
+    fun visit(e: Mul): String {
+        return "${e.left.apply(PrintVisitor())} * ${e.right.apply(PrintVisitor())}"
+    }
+}
+
+class CalculateVisitor {
+    fun visit(e: Var): Int {
+        return e.value
+    }
+
+    fun visit(e: Sum): Int {
+        return e.left.apply(CalculateVisitor()) + e.right.apply(CalculateVisitor())
+    }
+
+    fun visit(e: Mul): Int {
+        return e.left.apply(CalculateVisitor()) * e.right.apply(CalculateVisitor())
     }
 }
 
 fun main() {
-    println("${Mul(Sum(Var(2), Var(3)), Var(4))} = ${Mul(Sum(Var(2), Var(3)), Var(4)).calc()}")
-    println("${Mul(Sum(Sum(Var(2), Var(2)), Mul(Sum(Mul(Var(2), Var(4)), Var(3)), Var(4))), Var(10))} = ${Mul(Sum(Sum(Var(2), Var(2)), Mul(Sum(Mul(Var(2), Var(4)), Var(3)), Var(4))), Var(10)).calc()}")
-    println("${Var(5)} = ${Var(5).calc()}")
-    println("${Sum(Var(2), Var(3))} = ${Sum(Var(2), Var(3)).calc()}")
-    println("${Mul(Var(2), Var(2))} = ${Mul(Var(2), Var(2)).calc()}")
-    println("${Mul(Var(3), Sum(Var(4), Var(5)))} = ${Mul(Var(3), Sum(Var(4), Var(5))).calc()}")
-    println("${Sum(Mul(Var(3), Var(3)), Mul(Var(4), Var(4)))} = ${Sum(Mul(Var(3), Var(3)), Mul(Var(4), Var(4))).calc()}")
-    println("${Sum(Sum(Sum(Sum(Var(1), Var(2)), Var(3)), Var(4)), Var(5))} = ${Sum(Sum(Sum(Sum(Var(1), Var(2)), Var(3)), Var(4)), Var(5)).calc()}")
+    println("${Mul(Sum(Var(2), Var(3)), Var(4)).apply(PrintVisitor())} = ${Mul(Sum(Var(2), Var(3)), Var(4)).apply(CalculateVisitor())}")
+    println("${Mul(Sum(Sum(Var(2), Var(2)), Mul(Sum(Mul(Var(2), Var(4)), Var(3)), Var(4))), Var(10)).apply(PrintVisitor())} = ${Mul(Sum(Sum(Var(2), Var(2)), Mul(Sum(Mul(Var(2), Var(4)), Var(3)), Var(4))), Var(10)).apply(CalculateVisitor())}")
+    println("${Var(5).apply(PrintVisitor())} = ${Var(5).apply(CalculateVisitor())}")
+    println("${Sum(Var(2), Var(3)).apply(PrintVisitor())} = ${Sum(Var(2), Var(3)).apply(CalculateVisitor())}")
+    println("${Mul(Var(2), Var(2)).apply(PrintVisitor())} = ${Mul(Var(2), Var(2)).apply(CalculateVisitor())}")
+    println("${Mul(Var(3), Sum(Var(4), Var(5))).apply(PrintVisitor())} = ${Mul(Var(3), Sum(Var(4), Var(5))).apply(CalculateVisitor())}")
+    println("${Sum(Mul(Var(3), Var(3)), Mul(Var(4), Var(4))).apply(PrintVisitor())} = ${Sum(Mul(Var(3), Var(3)), Mul(Var(4), Var(4))).apply(CalculateVisitor())}")
+    println("${Sum(Sum(Sum(Sum(Var(1), Var(2)), Var(3)), Var(4)), Var(5)).apply(PrintVisitor())} = ${Sum(Sum(Sum(Sum(Var(1), Var(2)), Var(3)), Var(4)), Var(5)).apply(CalculateVisitor())}")
 }
--- src/test/kotlin/mainTest.kt
+++ src/test/kotlin/mainTest.kt
@@ -6,161 +6,161 @@ internal class mainTest {
 
     @Test
     fun test1() {
-        assertEquals(-24824560, Var(-24824560).calc())
+        assertEquals(-24824560, Var(-24824560).apply(CalculateVisitor()))
     }
 
     @Test
     fun test2() {
-        assertEquals(-711545113, Var(-711545113).calc())
+        assertEquals(-711545113, Var(-711545113).apply(CalculateVisitor()))
     }
 
     @Test
     fun test3() {
-        assertEquals(-2008595430, Var(-2008595430).calc())
+        assertEquals(-2008595430, Var(-2008595430).apply(CalculateVisitor()))
     }
 
     @Test
     fun test4() {
-        assertEquals(413878301, Var(413878301).calc())
+        assertEquals(413878301, Var(413878301).apply(CalculateVisitor()))
     }
 
     @Test
     fun test5() {
-        assertEquals(643090450, Var(643090450).calc())
+        assertEquals(643090450, Var(643090450).apply(CalculateVisitor()))
     }
 
     @Test
     fun test6() {
-        assertEquals(815167056, Var(815167056).calc())
+        assertEquals(815167056, Var(815167056).apply(CalculateVisitor()))
     }
 
     @Test
     fun test7() {
-        assertEquals(-32539489, Var(-32539489).calc())
+        assertEquals(-32539489, Var(-32539489).apply(CalculateVisitor()))
     }
 
     @Test
     fun test8() {
-        assertEquals(-559234833, Var(-559234833).calc())
+        assertEquals(-559234833, Var(-559234833).apply(CalculateVisitor()))
     }
 
     @Test
     fun test9() {
-        assertEquals(-108838676, Sum(Var(28867047), Var(-137705723)).calc())
+        assertEquals(-108838676, Sum(Var(28867047), Var(-137705723)).apply(CalculateVisitor()))
     }
 
     @Test
     fun test10() {
-        assertEquals(605917576, Sum(Var(-79086392), Var(685003968)).calc())
+        assertEquals(605917576, Sum(Var(-79086392), Var(685003968)).apply(CalculateVisitor()))
     }
 
     @Test
     fun test11() {
-        assertEquals(779698151, Sum(Var(823832943), Var(-44134792)).calc())
+        assertEquals(779698151, Sum(Var(823832943), Var(-44134792)).apply(CalculateVisitor()))
     }
 
     @Test
     fun test12() {
-        assertEquals(126621482, Sum(Var(-581937098), Var(708558580)).calc())
+        assertEquals(126621482, Sum(Var(-581937098), Var(708558580)).apply(CalculateVisitor()))
     }
 
     @Test
     fun test13() {
-        assertEquals(-578918130, Sum(Var(-564960848), Var(-13957282)).calc())
+        assertEquals(-578918130, Sum(Var(-564960848), Var(-13957282)).apply(CalculateVisitor()))
     }
 
     @Test
     fun test14() {
-        assertEquals(234987134, Sum(Var(220557717), Var(14429417)).calc())
+        assertEquals(234987134, Sum(Var(220557717), Var(14429417)).apply(CalculateVisitor()))
     }
 
     @Test
     fun test15() {
-        assertEquals(1071829646, Sum(Var(964669627), Var(107160019)).calc())
+        assertEquals(1071829646, Sum(Var(964669627), Var(107160019)).apply(CalculateVisitor()))
     }
 
     @Test
     fun test16() {
-        assertEquals(653983164, Sum(Var(715982973), Var(-61999809)).calc())
+        assertEquals(653983164, Sum(Var(715982973), Var(-61999809)).apply(CalculateVisitor()))
     }
 
     @Test
     fun test17() {
-        assertEquals(321768, Mul(Var(-2952), Var(-109)).calc())
+        assertEquals(321768, Mul(Var(-2952), Var(-109)).apply(CalculateVisitor()))
     }
 
     @Test
     fun test18() {
-        assertEquals(-8783380, Mul(Var(2572), Var(-3415)).calc())
+        assertEquals(-8783380, Mul(Var(2572), Var(-3415)).apply(CalculateVisitor()))
     }
 
     @Test
     fun test19() {
-        assertEquals(-18813232, Mul(Var(4456), Var(-4222)).calc())
+        assertEquals(-18813232, Mul(Var(4456), Var(-4222)).apply(CalculateVisitor()))
     }
 
     @Test
     fun test20() {
-        assertEquals(-21063246, Mul(Var(4934), Var(-4269)).calc())
+        assertEquals(-21063246, Mul(Var(4934), Var(-4269)).apply(CalculateVisitor()))
     }
 
     @Test
     fun test21() {
-        assertEquals(-8619374, Mul(Var(-2686), Var(3209)).calc())
+        assertEquals(-8619374, Mul(Var(-2686), Var(3209)).apply(CalculateVisitor()))
     }
 
     @Test
     fun test22() {
-        assertEquals(24019497, Mul(Var(2781), Var(8637)).calc())
+        assertEquals(24019497, Mul(Var(2781), Var(8637)).apply(CalculateVisitor()))
     }
 
     @Test
     fun test23() {
-        assertEquals(42164672, Mul(Var(-6523), Var(-6464)).calc())
+        assertEquals(42164672, Mul(Var(-6523), Var(-6464)).apply(CalculateVisitor()))
     }
 
     @Test
     fun test24() {
-        assertEquals(-18670851, Mul(Var(7129), Var(-2619)).calc())
+        assertEquals(-18670851, Mul(Var(7129), Var(-2619)).apply(CalculateVisitor()))
     }
 
     @Test
     fun test25() {
-        assertEquals("(2 + 3) * 4", Mul(Sum(Var(2), Var(3)), Var(4)).toString())
+        assertEquals("(2 + 3) * 4", Mul(Sum(Var(2), Var(3)), Var(4)).apply(PrintVisitor()))
     }
 
     @Test
     fun test26() {
-        assertEquals("((2 + 2) + (2 * 4 + 3) * 4) * 10", Mul(Sum(Sum(Var(2), Var(2)), Mul(Sum(Mul(Var(2), Var(4)), Var(3)), Var(4))), Var(10)).toString())
+        assertEquals("((2 + 2) + (2 * 4 + 3) * 4) * 10", Mul(Sum(Sum(Var(2), Var(2)), Mul(Sum(Mul(Var(2), Var(4)), Var(3)), Var(4))), Var(10)).apply(PrintVisitor()))
     }
 
     @Test
     fun test27() {
-        assertEquals("5", Var(5).toString())
+        assertEquals("5", Var(5).apply(PrintVisitor()))
     }
 
     @Test
     fun test28() {
-        assertEquals("(2 + 3)", Sum(Var(2), Var(3)).toString())
+        assertEquals("(2 + 3)", Sum(Var(2), Var(3)).apply(PrintVisitor()))
     }
 
     @Test
     fun test29() {
-        assertEquals("2 * 2", Mul(Var(2), Var(2)).toString())
+        assertEquals("2 * 2", Mul(Var(2), Var(2)).apply(PrintVisitor()))
     }
 
     @Test
     fun test30() {
-        assertEquals("3 * (4 + 5)", Mul(Var(3), Sum(Var(4), Var(5))).toString())
+        assertEquals("3 * (4 + 5)", Mul(Var(3), Sum(Var(4), Var(5))).apply(PrintVisitor()))
     }
 
     @Test
     fun test31() {
-        assertEquals("(3 * 3 + 4 * 4)", Sum(Mul(Var(3), Var(3)), Mul(Var(4), Var(4))).toString())
+        assertEquals("(3 * 3 + 4 * 4)", Sum(Mul(Var(3), Var(3)), Mul(Var(4), Var(4))).apply(PrintVisitor()))
     }
 
     @Test
     fun test32() {
-        assertEquals("((((1 + 2) + 3) + 4) + 5)", Sum(Sum(Sum(Sum(Var(1), Var(2)), Var(3)), Var(4)), Var(5)).toString())
+        assertEquals("((((1 + 2) + 3) + 4) + 5)", Sum(Sum(Sum(Sum(Var(1), Var(2)), Var(3)), Var(4)), Var(5)).apply(PrintVisitor()))
     }
 }
