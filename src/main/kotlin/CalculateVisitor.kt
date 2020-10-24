444af7020659a2e0da2b61dc39709cbaa52e845f
sergeykhargelia <sergey.khargelia@gmail.com>
2020-10-25 00:49:32 +0300

    implemented calculateVisitor

---
+++ src/main/kotlin/CalculateVisitor.kt
@@ -0,0 +1,15 @@
+class CalculateVisitor: Visitor<Int> {
+    override fun visit(node: Literal): Int {
+        return node.value
+    }
+    override fun visit(node: Sum): Int {
+        return node.left.accept(CalculateVisitor()) + node.right.accept(CalculateVisitor())
+    }
+    override fun visit(node: Multiply): Int {
+        return node.left.accept(CalculateVisitor()) + node.right.accept(CalculateVisitor())
+    }
+}
+
+fun calculateExpression(node: Expression): Int {
+    return node.accept(CalculateVisitor())
+}
