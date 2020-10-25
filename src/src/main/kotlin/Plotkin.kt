78975c4783271a0a8122fd16f5ce82b1793c41b8
Roman Bukreev <romanbukreev@icloud.com>
2020-10-26 02:43:39 +0300

    Complete task 2

--- src/src/main/kotlin/Plotkin.kt
+++ src/src/main/kotlin/Plotkin.kt
@@ -2,6 +2,7 @@ import java.io.BufferedReader
 import java.io.FileReader
 
 typealias Instruction = Pair<Int, List<String>>
+typealias Range = Pair<Int, Int>
 
 fun readInstructions(): List<Instruction> {
     val fileReader: BufferedReader?
@@ -42,8 +43,8 @@ fun implement(initial: Int, instructions: List<Instruction>): Int {
     var variables = mutableListOf<Int>(initial, 0, 0) // variables[0] - x, variables[1] - y, variables[2] - z
     var index = 0
     while (index < instructions.count()) {
-        var instruction = instructions[index]
-        when (instruction.first) {
+        val instruction = instructions[index]
+        when (instruction.first) { // inc - 1, dec - 2, zero - 3, stop - 0
             0 -> return variables[1]
             1 -> {
                 variables = increment(instruction.second[0], variables)
@@ -61,9 +62,63 @@ fun implement(initial: Int, instructions: List<Instruction>): Int {
     return variables[1]
 }
 
+fun incrementRange(name: String, ranges: MutableList<Range>): MutableList<Range> {
+    ranges[nameToNumber(name)] = Pair(ranges[nameToNumber(name)].first, ranges[nameToNumber(name)].second + 1)
+    return ranges
+}
+
+fun decrementRange(name: String, ranges: MutableList<Range>): MutableList<Range> {
+    if (ranges[nameToNumber(name)].first > 0)
+    ranges[nameToNumber(name)] = Pair(ranges[nameToNumber(name)].first - 1, ranges[nameToNumber(name)].second)
+    return ranges
+}
+
+fun inRange(value: Int, range: Range): Boolean {
+    return value >= range.first && value <= range.second
+}
+
+fun zeroInRange(ranges: MutableList<Range>, name: String, ifTrue: Int, ifFalse: Int): Int {
+    return if (inRange(0, ranges[nameToNumber(name)])) ifTrue else ifFalse
+}
+
+fun implementRange(initial: Range, instructions: List<Instruction>): Range {
+    var ranges = mutableListOf<Range>(initial, Pair(0, 0), Pair(0, 0))
+    val maxNumberOfSteps = 100
+    var curNumberOfSteps = 0
+    var index = 0
+    while (curNumberOfSteps < maxNumberOfSteps && index < instructions.count()) {
+        val instruction = instructions[index]
+        when (instruction.first) {
+            0 -> return ranges[1]
+            1 -> {
+                ranges = incrementRange(instruction.second[0], ranges)
+                index += 1
+            }
+            2 -> {
+                ranges = decrementRange(instruction.second[0], ranges)
+                index += 1
+            }
+            3 -> {
+                index = zeroInRange(ranges, instruction.second[0], instruction.second[1].toInt(), instruction.second[3].toInt())
+            }
+        }
+        curNumberOfSteps += 1
+    }
+    return ranges[1]
+}
 
 fun main() {
-    println("Input initial value:")
-    val initial = readLine()!!.toInt()
-    println(implement(initial, readInstructions()))
+    println("Choose mode:\n1. For one value\n2. For Interval")
+    val mode = readLine()!!.toInt()
+    if (mode == 1) {
+        println("Input initial value:")
+        val initial = readLine()!!.toInt()
+        println(implement(initial, readInstructions()))
+    }
+    else {
+        println("Input initial Range(two numbers on separate lines):")
+        val from = readLine()!!.toInt()
+        val to = readLine()!!.toInt()
+        println(implementRange(Pair(from, to), readInstructions()))
+    }
 }
